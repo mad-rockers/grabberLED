fe2c40e166ca002978b837cba79639a44f381363
Joe McConnell <joe.mcconnell10@gmail.com>
2023-02-04 12:12:29 -0600

    working to detect colors and change to purple stat

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -7,7 +7,11 @@
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
-#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/smartdashboard/smartdashboard.h>
+#include <frc/util/color.h>
+#include "rev/ColorSensorV3.h"
+#include "rev/ColorMatch.h"
+
 
 /**
  * This sample program shows how to control a motor using a joystick. In the
@@ -24,17 +28,94 @@ class Robot : public frc::TimedRobot {
  public:
   void TeleopPeriodic() override { m_motor.Set(0.89); }
 
+  /**
+   * Change the I2C port below to match the connection of your color sensor
+   */
+  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+
+  /**
+   * A Rev Color Sensor V3 object is constructed with an I2C port as a 
+   * parameter. The device will be automatically initialized with default 
+   * parameters.
+   */
+  rev::ColorSensorV3 m_colorSensor{i2cPort};
+
+  /**
+   * A Rev Color Match object is used to register and detect known colors. This can 
+   * be calibrated ahead of time or during operation.
+   * 
+   * This object uses a simple euclidian distance to estimate the closest match
+   * with given confidence range.
+   */
+  rev::ColorMatch m_colorMatcher;
+
+  /**
+   * Note: Any example colors should be calibrated as the user needs, these
+   * are here as a basic example.
+   */
+  static constexpr frc::Color kBlueTarget = frc::Color(0.143, 0.427, 0.429);
+  static constexpr frc::Color kGreenTarget = frc::Color(0.197, 0.561, 0.240);
+  static constexpr frc::Color kRedTarget = frc::Color(0.561, 0.232, 0.114);
+  static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
+  static constexpr frc::Color kPurpleTarget = frc::Color(0.204, 0.292, 0.518);
+
   /*
    * The RobotPeriodic function is called every control packet no matter the
    * robot mode.
    */
   void RobotPeriodic() override {
+    /**
+     * The method GetColor() returns a normalized color value from the sensor and can be
+     * useful if outputting the color to an RGB LED or similar. To
+     * read the raw color, use GetRawColor().
+     * 
+     * The color sensor works best when within a few inches from an object in
+     * well lit conditions (the built in LED is a big help here!). The farther
+     * an object is the more light from the surroundings will bleed into the 
+     * measurements and make it difficult to accurately determine its color.
+     */
+    frc::Color detectedColor = m_colorSensor.GetColor();
     
+       /**
+     * Run the color match algorithm on our detected color
+     */
+    std::string colorString;
+    double confidence = 0.0;
+    frc::Color matchedColor = m_colorMatcher.MatchClosestColor(detectedColor, confidence);
+
+    if (matchedColor == kBlueTarget) {
+      colorString = "Cube";
+    } else if (matchedColor == kRedTarget) {
+      colorString = "Red";
+    } else if (matchedColor == kGreenTarget) {
+      colorString = "Green";
+    } else if (matchedColor == kYellowTarget) {
+      colorString = "Cone";
+    } else if (matchedColor == kPurpleTarget) {
+      colorString = "Cube";
+    } else {
+      colorString = "Unknown";
+    }
+
+    /**
+     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
+     * sensor.
+     */
+    frc::SmartDashboard::PutNumber("Red", detectedColor.red);
+    frc::SmartDashboard::PutNumber("Green", detectedColor.green);
+    frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
+    frc::SmartDashboard::PutNumber("Confidence", confidence);
+    frc::SmartDashboard::PutString("Detected Color", colorString);
   }
 
   void RobotInit() override {
     // Use SetDistancePerPulse to set the multiplier for GetDistance
     // This is set up assuming a 6 inch wheel with a 360 CPR encoder.
+    m_colorMatcher.AddColorMatch(kBlueTarget);
+    m_colorMatcher.AddColorMatch(kGreenTarget);
+    m_colorMatcher.AddColorMatch(kRedTarget);
+    m_colorMatcher.AddColorMatch(kYellowTarget);
+    m_colorMatcher.AddColorMatch(kPurpleTarget);
   }
 
  private:
