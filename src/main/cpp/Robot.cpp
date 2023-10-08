dadd6d48bef2d64ee9dc48a27f5ceed3db29117e
Joe McConnell <joe.mcconnell10@gmail.com>
2023-10-08 16:01:47 -0500

    updated with VictorSP

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -7,6 +7,7 @@
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
+#include <frc/motorcontrol/VictorSP.h>
 #include <frc/smartdashboard/smartdashboard.h>
 
 class Robot : public frc::TimedRobot {
@@ -33,11 +34,13 @@ class Robot : public frc::TimedRobot {
 
   void setSpeed() {
     ledStrip0.Set(0.69); 
+    victor.Set(0.6); // the % output of the motor, between -1 and 1
 
     }
 
  private:
   frc::PWMSparkMax ledStrip0{0}; //Configures PWM port 0 for motor
+  frc::VictorSP victor{1}; // 0 is the RIO PWM port this is connected to
 };
 
 #ifndef RUNNING_FRC_TESTS
